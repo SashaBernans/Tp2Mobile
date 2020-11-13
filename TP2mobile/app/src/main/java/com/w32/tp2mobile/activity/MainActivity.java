90e6373cf7bb410502b8917f4e69ad14c82a29f9
Iblisec <iblisec@gmail.com>
2020-11-13 16:15:55 -0500

    jsp

--- TP2mobile/app/src/main/java/com/w32/tp2mobile/activity/MainActivity.java
+++ TP2mobile/app/src/main/java/com/w32/tp2mobile/activity/MainActivity.java
@@ -6,17 +6,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.w32.tp2mobile.R;
+import com.w32.tp2mobile.model.SensorValue;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView message;
+    private SensorData temperatureData = new SensorData(SensorID)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         message= findViewById(R.id.tvMessages);
+
+
+        InputStream theJSON = new FileInputStream("humidity.json");
+         = objectMapper.readValue(theJSON, Individu.class);
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            SensorValue sensorValue = objectMapper.readValue(theJSON, SensorValue.class);
+        } catch (JsonParseException | JsonMappingException e) {
+            e.printStackTrace();
+        }
     }
 
     public void onStartTempeture(){
